33984529afdd86af76d0f1ebd7695c1ae2f64a2d
WittySmirk <wyattdegenhart@gmail.com>
2026-04-03 22:23:10 -0600

    finished picture component, upload images start

--- src/lib/components/picture.svelte
+++ src/lib/components/picture.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
 	import Draggable from './draggable.svelte';
+
+	let {
+		src = 'https://external-preview.redd.it/so-my-friend-introduced-me-to-the-theory-that-pepe-silvia-v0-rHlx3CX3RCXzhXTxnPX5ejpEmjXQjLRJjCPheSpx7Qk.jpg?width=640&crop=smart&auto=webp&s=b469871644e6ed5307b24c501f5747e3be9cb2a4'
+	} = $props();
 </script>
 
 <Draggable>
-	<img
-		src="https://external-preview.redd.it/so-my-friend-introduced-me-to-the-theory-that-pepe-silvia-v0-rHlx3CX3RCXzhXTxnPX5ejpEmjXQjLRJjCPheSpx7Qk.jpg?width=640&crop=smart&auto=webp&s=b469871644e6ed5307b24c501f5747e3be9cb2a4"
-	/>
+	<img {src} />
 </Draggable>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,20 @@
 <script lang="ts">
 	import Draggable from '$lib/components/draggable.svelte';
 	import Picture from '$lib/components/picture.svelte';
+
+	let pictures: string[] = $state([]);
+
+	function onchange(e: Event & { currentTarget: HTMLInputElement }) {
+		const files = e.currentTarget.files;
+		if (files != null) {
+			[...files].map((file) => {
+				if (file.type.includes('image')) {
+					const src = URL.createObjectURL(file);
+					pictures.push(src);
+				}
+			});
+		}
+	}
 </script>
 
 <head>
@@ -14,3 +28,13 @@
 </Draggable>
 
 <Picture></Picture>
+
+{#if pictures.length != 0}
+	{#each pictures as picture}
+		<Picture src={picture}></Picture>
+	{/each}
+{/if}
+
+<!-- TODO: add styling to upload button -->
+<input type="file" id="upload" {onchange} hidden />
+<label for="upload">Upload File</label>
