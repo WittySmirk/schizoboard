efeb3ebf01eeae2de38dafbba1f9a0922fcd00a7
WittySmirk <wyattdegenhart@gmail.com>
2026-04-03 21:08:05 -0600

    added drag/drop

---
+++ src/lib/components/draggable.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	let { initialX = 100, initialY = 100 } = $props();
+	let x: number = $state(initialX);
+	let y: number = $state(initialY);
+
+	let down: boolean = false;
+
+	function onmousemove(e) {
+		if (down) {
+			x = e.clientX;
+			y = e.clientY;
+		}
+	}
+	function onmousedown() {
+		down = true;
+	}
+	function onmouseup() {
+		down = false;
+	}
+</script>
+
+<!-- TODO: props -->
+<div {onmousedown} class="absolute bg-blue-500" style:top="{y}px" style:left="{x}px">
+	<h1>hello diddy</h1>
+</div>
+
+<svelte:window {onmousemove} {onmouseup} />
